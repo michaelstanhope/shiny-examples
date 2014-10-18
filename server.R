80428ef09bdc5e537d9adb3c12520a7ce429a215
Michael Stanhope <michael.stanhope@rittmanmead.com>
2014-10-18 14:37:04 +0100

    Initial dev
    Built the student loan repayment calculator

---
+++ server.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(googleVis)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    output$repaymentPlot <- renderGvis({
+        annualInterest <- input$annualInterest
+        annualIncrease <- input$annualIncrease
+        startValue <-input$startValue
+        startYear <- input$startYear
+        startSalary <- input$startSalary
+        month <- 1:301
+        minSalary <-16910
+        monthlyIncrease <- ((annualIncrease/100)+1)^(1/12)
+        monthlyInterest <- ((annualInterest/100)+1)^(1/12) -1
+        balance <- 0
+        monthlyPayments <- 0
+        monthlyPayments[month] <- (startSalary*(monthlyIncrease^(month)) - 21000)*0.09/12
+        monthlyPayments[monthlyPayments < 0] <- 0
+        monthlyPayments[1:8] <- 0
+        total <- 0
+        
+        balance[1] <- startValue
+        for(i in 2:length(month))
+        {
+            balance[i] <- balance[i-1] + monthlyInterest*balance[i-1] - monthlyPayments[i]
+            total <- total + max(monthlyInterest*balance[i-1],0)
+        }
+        balance <- balance[balance > 0]
+        monthlyPayments<-monthlyPayments[1:length(balance)]
+        df=data.frame(years=round((index(balance)-1)/12,2), Balance=round(balance), Payments=round(monthlyPayments))
+        
+        output$time <- renderText({length(balance)})
+        output$total <- renderText({paste("£",as.character(round(total)),sep="")})
+        
+        startDate <- as.POSIXct(strptime(paste("01-09-",startYear, sep=""), "%d-%m-%Y"))
+        endDate <- startDate
+        month(endDate) <- month(endDate) + length(balance)
+        output$finalMonth <- renderText({paste(as.character(endDate, format="%B %Y"),".",sep="")})
+                                             
+        gvisLineChart(df, 
+                      xvar="years", 
+                      yvar=c("Balance","Payments"), 
+                      options=list(
+                          width=600,
+                          height=250,
+                          #title="Outstanding Balance and Monthly Payments",
+                          hAxes="[{title:'Time in Employment (Years)'}]",
+                          series="[{targetAxisIndex: 0},{targetAxisIndex:1}]",
+                          vAxes="[{title:'Balance (£)'}, {title:'Monthly Payments (£)'}]"
+                          ))                                        
+     })
+    
+    
+    
+})
---
+++ ui.R
@@ -0,0 +1,72 @@
+library(shiny)
+
+shinyUI(fluidPage(theme = "css/bootstrap.css",
+                  
+    titlePanel("UK Student Loan Repayment Estimator by Michael Stanhope"),
+    p("This calculator can be used to estimate the repayment period for a UK student loan. It is designed for UK nationals who started university in 2006."),
+    p(strong("How to use this application:", inline=TRUE), " It's easy - just enter your loan amount, graduation year and assumptions on the left of the page."),
+    p("The calculator assumes that the student starts permanent employment in the Sepetember of their graduation year and starts repaying their loan no earlier than the following April. Salary increases and interest payments are applied each month so ",
+      strong("do not attempt to derive an accurate final payment date using this tool!", inline=TRUE),
+      " In reality, interest is calculated daily and most of us get bigger, less frequent pay rises."),
+    
+    # Sidebar with a slider input for the number of bins
+    sidebarLayout(
+        sidebarPanel(width=3,
+            sliderInput("startValue",
+                        "Initial Loan Amount (£):",
+                        min = 3000,
+                        max = 50000,
+                        value = 25000,
+                        step = 100),
+            sliderInput("startSalary",
+                        "Starting Salary (£):",
+                        min = 10000,
+                        max = 100000,
+                        value = 25000,
+                        step = 100),
+            sliderInput("annualInterest",
+                        "Annual Interest Rate (%):",
+                        min = 0,
+                        max = 5,
+                        value = 1.5,
+                        step = 0.5,
+                        round = -2),
+            sliderInput("annualIncrease",
+                        "Annual Salary Increase (%):",
+                        min = 0,
+                        max = 100,
+                        value = 4,
+                        step = 1),
+            sliderInput("startYear",
+                        "Graduation Year:",
+                        min = 2009,
+                        max = 2011,
+                        value = 2010,
+                        step = 1,
+                        format = "####")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            div("Using these assumptions, you can expect to pay off your loan  after approximately ",
+                strong(textOutput("time", inline=TRUE)), 
+                strong(" monthly paydays,"), 
+                " giving you a final repayment date of",
+                strong(textOutput("finalMonth", inline=TRUE)),
+                " You will pay approximately ",
+                strong(textOutput("total", inline=TRUE)),
+                " in interest over the period (doh!)."
+                ),
+            div(htmlOutput("repaymentPlot")),
+            div("You will start repaying your loan at the beginning of the tax year following your graduation,",
+                " assuming your earn at least ",
+                strong("£16910", inline=TRUE),
+                " per year. Your annual payments will be ",
+                strong("9%", inline=TRUE),
+                " of anything above this amount. ",
+                "If you have not repaid the balance within ",
+                strong("25 years", inline=TRUE),
+                " of graduating, the loan will be written-off.")
+        )       
+    )
+))
